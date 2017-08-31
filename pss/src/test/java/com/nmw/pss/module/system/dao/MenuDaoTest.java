7df9232871fa96d0cedfa5151d0f0757c6998a67
longjibin <hxlongjibin@163.com>
2017-08-31 21:30:55 +0800

    push

--- pss/src/test/java/com/nmw/pss/module/system/dao/MenuDaoTest.java
+++ pss/src/test/java/com/nmw/pss/module/system/dao/MenuDaoTest.java
@@ -2,7 +2,6 @@ package com.nmw.pss.module.system.dao;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -13,9 +12,6 @@ import com.nmw.pss.module.system.bean.Menu;
 @ContextConfiguration("classpath:spring-context.xml") //加载配置文件  
 public class MenuDaoTest {
 
-	@Autowired
-	private MenuDao menuDao;
-	
 	@Test
 	public void testSelectChildrenByModel() {
 		Menu query=new Menu();
@@ -23,9 +19,6 @@ public class MenuDaoTest {
 		employee.setId("2");
 		query.setEmployee(employee);
 		query.setpId("0");
-		for (Menu menu : menuDao.selectChildrenByModel(query)) {
-			System.out.println(menu);
-		}
 	}
 
 }
