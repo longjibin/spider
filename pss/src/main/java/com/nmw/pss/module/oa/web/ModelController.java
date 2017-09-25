ed58b11bf15efb48b4873b21db35b289fbd69969
longjibin <hxlongjibin@163.com>
2017-09-25 22:55:25 +0800

    push

--- pss/src/main/java/com/nmw/pss/module/oa/web/ModelController.java
+++ pss/src/main/java/com/nmw/pss/module/oa/web/ModelController.java
@@ -1,21 +1,24 @@
 package com.nmw.pss.module.oa.web;
 
+import java.util.HashMap;
 import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
 
 import org.activiti.editor.constants.ModelDataJsonConstants;
 import org.activiti.engine.RepositoryService;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.nmw.pss.common.constant.HttpConstant;
 
 /**
  * 模型控制器
@@ -25,6 +28,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 @Controller
 @RequestMapping(value = "${adminPath}/model")
 public class ModelController {
+	private static final Logger LOGGER=LoggerFactory.getLogger(ModelController.class);
 
 	@Autowired
 	private RepositoryService repositoryService;
@@ -40,17 +44,21 @@ public class ModelController {
 		return "oa/model/modellist";
 	}
 	
-	@RequestMapping(value = "create", method = RequestMethod.GET, produces = "application/json;charset=utf-8")
-	public void create(@RequestParam("name") String name, @RequestParam("key") String key,
-			@RequestParam(value = "description", required = false) String description, HttpServletRequest request,
-			HttpServletResponse response) {
+	@RequestMapping(value="form")
+	public String menuForm(String modelId, Model model){
+		return "oa/model/modelform";
+	}
+	
+	@ResponseBody
+	@RequestMapping(value = "create", method = RequestMethod.POST, produces = "application/json;charset=utf-8")
+	public Map<String, Object> create(String name, String key, String description) {
+		Map<String, Object> resultMap=new HashMap<String, Object>();
 		try {
 			ObjectMapper objectMapper = new ObjectMapper();
 			ObjectNode modelObjectNode = objectMapper.createObjectNode();
 			modelObjectNode.put(ModelDataJsonConstants.MODEL_NAME, name);
 			modelObjectNode.put(ModelDataJsonConstants.MODEL_REVISION, 1);
-			modelObjectNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION,
-					org.apache.commons.lang3.StringUtils.defaultString(description));
+			modelObjectNode.put(ModelDataJsonConstants.MODEL_DESCRIPTION, StringUtils.defaultString(description));
 			org.activiti.engine.repository.Model newModel = repositoryService.newModel();
 			newModel.setMetaInfo(modelObjectNode.toString());
 			newModel.setName(name);
@@ -63,9 +71,30 @@ public class ModelController {
 			stencilSetNode.put("namespace", "http://b3mn.org/stencilset/bpmn2.0#");
 			editorNode.put("stencilset", stencilSetNode);
 			repositoryService.addModelEditorSource(newModel.getId(), editorNode.toString().getBytes("utf-8"));
-			response.sendRedirect(request.getContextPath() + "/modeler.html?modelId=" + newModel.getId());
+			resultMap.put(HttpConstant.HTTP_CODE_KEY, HttpConstant.HTTP_CODE_200);
+			resultMap.put(HttpConstant.HTTP_MSG_KEY, HttpConstant.HTTP_MSG_200);
+			resultMap.put(HttpConstant.HTTP_DATA_KEY, "modeler.html?modelId=" + newModel.getId());
 		} catch (Exception e) {
-			e.getStackTrace();
+			resultMap.put(HttpConstant.HTTP_CODE_KEY, HttpConstant.HTTP_CODE_500);
+			resultMap.put(HttpConstant.HTTP_MSG_KEY, "操作失败:"+e.getMessage());
+			LOGGER.warn("模型保存失败", e);
 		}
+		return resultMap;
+	}
+	
+	@ResponseBody
+	@RequestMapping(value="remove",method=RequestMethod.GET)
+	public Map<String, Object> menuRemove(String modelId){
+		Map<String, Object> resultMap=new HashMap<String, Object>();
+		try {
+			repositoryService.deleteModel(modelId);
+			resultMap.put(HttpConstant.HTTP_CODE_KEY, HttpConstant.HTTP_CODE_200);
+			resultMap.put(HttpConstant.HTTP_MSG_KEY, HttpConstant.HTTP_MSG_200);
+		} catch (Exception e) {
+			resultMap.put(HttpConstant.HTTP_CODE_KEY, HttpConstant.HTTP_CODE_500);
+			resultMap.put(HttpConstant.HTTP_MSG_KEY, "操作失败:"+e.getMessage());
+			LOGGER.warn("模型删除失败", e);
+		}
+		return resultMap;
 	}
 }
--- pss/src/main/webapp/WEB-INF/views/oa/model/modellist.jsp
+++ pss/src/main/webapp/WEB-INF/views/oa/model/modellist.jsp
@@ -6,6 +6,7 @@
 	<ol class="breadcrumb">
 		<li><a href="#"><i class="fa fa-dashboard"></i> 首页</a></li>
 		<li><a href="#">办公中心</a></li>
+		<li><a href="#">模型管理</a></li>
 		<li class="active">模型列表</li>
 	</ol>
 </section>
@@ -15,28 +16,36 @@
 		<div class="col-md-12">
 			<div class="nav-tabs-custom">
 				<ul class="nav nav-tabs">
-					<li class="active"><a href="javascript:loadPage('role/model','GET',null);">模型列表</a></li>
+					<li class="active"><a href="javascript:loadPage('model/list','GET',null);">模型列表</a></li>
+					<shiro:hasPermission name="oa:model:edit">
+						<li><a href="javascript:loadPage('model/form','GET',null);">模型设计</a></li>
+					</shiro:hasPermission>
 				</ul>
 				<div class="tab-content">
 					<div class="active tab-pane">
 						<div class="box box-info">
-							<!-- /.box-header -->
 							<div class="box-body no-padding">
 								<table id="tabletree" class="table table-hover">
 									<tbody>
 										<tr>
 											<th width="50px;">序号</th>
-											<th>角色名</th>
+											<th>模型id</th>
+											<th>名称</th>
 											<th>操作</th>
 										</tr>
 										<c:forEach items="${list }" var="model" varStatus="s">
 											<tr>
 												<td>${s.count }</td>
-												<td>${role.id }</td>
-												<td>${role.name }</td>
-												<%-- <td width="150px;">
-													<a href="javascript:loadPage('role/form?id=${role.id }','GET',null);"><i class="fa fa-edit"></i> ${employee.loginName eq fns:getConfig('adminAccount')?'修改':'查看' }</a>&nbsp;&nbsp;&nbsp;&nbsp;
-												</td> --%>
+												<td>${model.id }</td>
+												<td>${model.name }</td>
+												<td width="150px;">
+													<shiro:hasPermission name="oa:model:edit">
+														<a href="${ctx }/modeler.html?modelId=${model.id}" target="_blank"><i class="fa fa-edit"></i> 编辑</a>&nbsp;&nbsp;&nbsp;&nbsp;
+													</shiro:hasPermission>
+													<shiro:hasPermission name="oa:model:delete">
+														<a href="javascript:remove('model/remove?modelId=${model.id }','model/list');"><i class="fa fa-edit"></i> 删除</a>&nbsp;&nbsp;&nbsp;&nbsp;
+													</shiro:hasPermission>
+												</td>
 											</tr>
 										</c:forEach>
 									</tbody>
