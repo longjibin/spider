3bbff5916515c37685c58f73a3b9e15e88d788f6
龙继斌 <hxlongjibin@travelsky.com>
2017-11-03 17:07:54 +0800

    push

---
+++ spider/src/main/java/com/lgb/webspider/jd/script/OtherScript.java
@@ -0,0 +1,15 @@
+package com.lgb.webspider.jd.script;
+
+import org.openqa.selenium.WebDriver;
+
+import com.lgb.common.downloader.Script;
+
+public class OtherScript implements Script {
+
+	@Override
+	public void script(WebDriver webDriver) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
